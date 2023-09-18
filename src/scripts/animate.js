d62a07bc1e649c69da75827de0bb8f6c7b28f66a
Shannon Millar <shanenak@gmail.com>
2023-09-18 14:34:27 -0700

    Switch to set interval instead of request animation

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -10,45 +10,32 @@ class Animate {
 
     static PRESSED_FRAMES = 25;
     static DIST_THRESHOLD = 25;
+
     static BPM = 110;
     static SEC_PER_MIN = 60;
-    static BEATS_PER_SEC = Animate.BPM / Animate.SEC_PER_MIN;
+    static FPS = 24;
+    static FRAMES_PER_BEAT = 13;
 
-    static FRAMES_PER_BEAT = 20;
-    static BEATS_PER_FRAME = 1/Animate.FRAMES_PER_BEAT;
     
     startGame() {
         this.canvas.addTargets();   
-        this.startAnimating(3 * (Animate.BEATS_PER_SEC/Animate.BEATS_PER_FRAME));
+        this.startAnimating(Animate.SEC_PER_MIN / Animate.FPS);
     };
 
     startAnimating(fps) {
         this.frameCount = 0;
-        this.fpsInterval = 1000 / fps;
-        this.then = window.performance.now();
-        this.startTime = this.then;
-        this.animate();
+        this.interval = setInterval(this.animate.bind(this),fps)
     };
 
     animate() {
-        if (this.canvas.arrows.length < 10) window.requestAnimationFrame(this.animate.bind(this));
-
-        let now = Date.now();
-        let elapsed = now - this.then;
-        if (elapsed > this.fpsInterval) {
-            this.then = now - (elapsed % this.fpsInterval);
-
-            this.ctx.clearRect(0,0,DIM_X, DIM_Y)
-            if (this.frameCount === 0 || this.frameCount % 20 === 0) {
-            this.canvas.createArrow();
-        }
-            this.canvas.draw();
-            this.canvas.update();
-
-            var sinceStart = now - this.startTime;
-            var currentFps = Math.round(1000 / (sinceStart / ++this.frameCount) * 100) / 100;
-            const fps = document.querySelector("#FPS")
-            fps.innerText = "Elapsed time= " + Math.round(sinceStart / 1000 * 100) / 100 + " secs @ " + currentFps + " fps.";
+        this.ctx.clearRect(0,0,DIM_X, DIM_Y)
+        if (this.frameCount % Animate.FRAMES_PER_BEAT === 0) this.canvas.createArrow();
+        this.canvas.draw();
+        this.canvas.update();
+        this.frameCount+=1;
+        if (this.frameCount > 24*15) {
+            clearInterval(this.interval);
+            console.log('interval cleared!');
         }
     }
 }
